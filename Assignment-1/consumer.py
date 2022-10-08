18731a05efeaabee27c3b7b9f81e391631a30d71
Shashanknew47 <shashank.edu@gmail.com>
2022-10-08 19:39:20 +0530

    new producer consumer file.

--- Assignment-1/consumer.py
+++ Assignment-1/consumer.py
@@ -1,67 +1,25 @@
+import client
+import file_object
+import schema
 
 
+from confluent_kafka.schema_registry import SchemaRegistryClient
 from confluent_kafka import Consumer
 from confluent_kafka.serialization import SerializationContext, MessageField
 from confluent_kafka.schema_registry.json_schema import JSONDeserializer
 
 
-from dotenv import load_dotenv
-import os
-
-
-load_dotenv()
-
-
-def sasl_conf():
-
-    sasl_conf = {'sasl.mechanism': os.environ["SSL_MACHENISM"],
-                 # Set to SASL_SSL to enable TLS support.
-                #  'security.protocol': 'SASL_PLAINTEXT'}
-                'bootstrap.servers':os.environ["BOOTSTRAP_SERVER"],
-                'security.protocol': os.environ["SECURITY_PROTOCOL"],
-                'sasl.username': os.environ["API_KEY"],
-                'sasl.password': os.environ["API_SECRET_KEY"]
-                }
-    return sasl_conf
-
-
-def schema_config():
-    return {'url':os.environ["ENDPOINT_SCHEMA_URL"],
-
-    'basic.auth.user.info':f"""{os.environ["SCHEMA_REGISTRY_API_KEY"]}:{os.environ["SCHEMA_REGISTRY_API_SECRET"]}"""
-
-    }
-
-
-class Restaurant:
-    def __init__(self,record) -> None:
-        for k,v in record.items():
-            self.k = v
-
-        self.record = record
-
-    def dict_to_restaurant(self,data,ctx):
-        return Restaurant(record=data)
-
-    def __str__(self) -> str:
-        return f'Restaurant({self.record})'
-
-
-
 
 def main(topic):
 
+    schema_str = schema.create_schema_str_latest('restaurent-take-away-data-value')
 
-    schema_registry_conf = schema_config()
-    schema_registry_client = SchemaRegistryClient(schema_registry_conf)
-    subject = schema_registry_client.get_subjects()
-    schema_str = schema_registry_client.get_latest_version('restaurent-take-away-data-value').schema.schema_str
 
 
     json_deserializer = JSONDeserializer(schema_str,
-                                         from_dict=Restaurant.dict_to_car)
+                                         from_dict=file_object.Restaurant.dict_to_restaurant)
 
-    consumer_conf = sasl_conf()
+    consumer_conf = client.sasl_conf()
     consumer_conf.update({
                      'group.id': 'group1',
                      'auto.offset.reset': "earliest"})
--- Assignment-1/producer.py
+++ Assignment-1/producer.py
@@ -1,109 +1,14 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Copyright 2020 Confluent Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import client
+import file_object
+import schema
 
 
-
-# A simple example demonstrating use of JSONSerializer.
-import os
 from uuid import uuid4
-import csv
-
-
 from confluent_kafka import Producer
 from confluent_kafka.schema_registry import SchemaRegistryClient
 from confluent_kafka.schema_registry.json_schema import JSONSerializer
 from confluent_kafka.serialization import (MessageField, SerializationContext,
                                            StringSerializer)
-from dotenv import load_dotenv
-
-
-load_dotenv()
-
-FILE_PATH = "/Users/shashankjain/Desktop/Practice/Ineuron/Kafka/DE_Kafka_project_assignment/Assignment-1/restaurant_orders.csv"
-
-
-def sasl_conf():
-
-    sasl_conf = {'sasl.mechanism': os.environ["SSL_MACHENISM"],
-                 # Set to SASL_SSL to enable TLS support.
-                #  'security.protocol': 'SASL_PLAINTEXT'}
-                'bootstrap.servers':os.environ["BOOTSTRAP_SERVER"],
-                'security.protocol': os.environ["SECURITY_PROTOCOL"],
-                'sasl.username': os.environ["API_KEY"],
-                'sasl.password': os.environ["API_SECRET_KEY"]
-                }
-    return sasl_conf
-
-
-def schema_config():
-    return {'url':os.environ["ENDPOINT_SCHEMA_URL"],
-
-    'basic.auth.user.info':f"""{os.environ["SCHEMA_REGISTRY_API_KEY"]}:{os.environ["SCHEMA_REGISTRY_API_SECRET"]}"""
-
-    }
-
-
-class Restaurant:
-    def __init__(self,record) -> None:
-        for k,v in record.items():
-            self.k = v
-
-        self.record = record
-
-    def dict_to_restaurant(self,data,ctx):
-        return Restaurant(record=data)
-
-    def __str__(self) -> str:
-        return f'Restaurant({self.record})'
-
-
-
-def cast_str_int(d):
-    new_di = {}
-    for k,v in d.items():
-        if k in ['Quantity','Total Products']:
-            new_di[k] = int(v)
-        elif k in ['Product Price']:
-            new_di[k] = float(v)
-
-        else:
-            new_di[k] = v
-
-    return new_di
-
-def get_restaurant_instance(filepath):
-    with open(filepath,'r') as file:
-        f = csv.DictReader(file)
-        for i in f:
-            i = cast_str_int(i)
-            yield (Restaurant(i))
-
-
-
-# def get_car_instance(file_path):
-#     df=pd.read_csv(file_path)
-#     df=df.iloc[:,1:]
-#     cars:List[Car]=[]
-#     for data in df.values:
-#         car=Car(dict(zip(columns,data)))
-#         cars.append(car)
-#         yield car
-
-
 
 def restaurant_to_dict(restaurant, ctx):
     """
@@ -118,6 +23,8 @@ def restaurant_to_dict(restaurant, ctx):
 
     # User._address must not be serialized; omit from dict
     return restaurant.record
+
+
 
 
 def delivery_report(err, msg):
@@ -139,23 +46,26 @@ def delivery_report(err, msg):
 
 def main(topic):
 
-    schema_registry_conf = schema_config()
-    schema_registry_client = SchemaRegistryClient(schema_registry_conf)
-    subject = schema_registry_client.get_subjects()
-    schema_str = schema_registry_client.get_latest_version('restaurent-take-away-data-value').schema.schema_str
+    schema_str = schema.create_schema_str_latest('restaurent-take-away-data-value')
+    schema_registry_client = SchemaRegistryClient(client.schema_config())
 
 
     string_serializer = StringSerializer('utf_8')
     json_serializer = JSONSerializer(schema_str, schema_registry_client, restaurant_to_dict)
 
-    producer = Producer(sasl_conf())
+
+    producer = Producer(client.sasl_conf())
 
     print("Producing user records to topic {}. ^C to exit.".format(topic))
     #while True:
         # Serve on_delivery callbacks from previous calls to produce()
+
+
     producer.poll(0.0)
+
     try:
-        for restaurant in get_restaurant_instance(filepath=FILE_PATH):
+        FILE_PATH = "/Users/shashankjain/Desktop/Practice/Ineuron/Kafka/DE_Kafka_project_assignment/Assignment-1/restaurant_orders.csv"
+        for restaurant in file_object.get_restaurant_instance(filepath=FILE_PATH):
 
             print(restaurant)
             producer.produce(topic=topic,
@@ -171,6 +81,7 @@ def main(topic):
 
     print("\nFlushing records...")
     producer.flush()
+
 
 
 main("restaurent-take-away-data")
